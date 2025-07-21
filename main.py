bdb8ea00106cc0ef0c8558eb7782ea26cfaf8432
nenaplnk <nenaplnk97@gmail.com>
2025-07-21 14:26:20 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from model import *
-from warnings import filterwarnings
-filterwarnings("ignore")
-sampling_params = SamplingParams(temperature=0.3, max_tokens=200, stop=['[/ANSWER]'])
- 
-if __name__ == "__main__":
-  llm = LLM(model="Qwen/Qwen2-7B-Instruct")
-  assistant = ArXivAssistant(llm, sampling_params)
-  assistant.run()
