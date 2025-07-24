4b037b8af79e71c1bd8620e9869e00d3a91c2544
nenaplnk <nenaplnk97@gmail.com>
2025-07-24 22:50:00 +0300

    Delete sc.sh

--- sc.sh
+++
@@ -1,8 +0,0 @@
-
-    !python3 -m vllm.entrypoints.openai.api_server \
-    --model Qwen/Qwen2-7B-Instruct \
-    --host 0.0.0.0 \
-    --port 8000 \
-    --max-model-len 2048 \
-    --download-dir /content/models \
-    --trust-remote-code
